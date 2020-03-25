b7b44aa3942fa105681c671351d5017a16a9bedb
Gary Liang <gary.liang@nab.com.au>
2020-03-25 16:42:54 +1100

    play

--- src/App.js
+++ src/App.js
@@ -1,104 +1,59 @@
-import React, {useState, useEffect, useRef, createRef, useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 
-const AppContext = React.createContext({
-  name: 'AppContext'
-});
-
-function createMenuItemRefs(items, rowInd) {
-  // obj
-  let menuItemRefs = {};
-  // loop each
-  for (let i = 0; i < Object.keys(items).length; i++) {
-    // When assign createRef, no current
-    menuItemRefs[rowInd + i] = createRef();
-  }
-  return menuItemRefs;
-}
-
-function Menu({buttonName, parentRowIndex}) {
-  const [currRowInd, setCurrRowInd] = useState('');
+function Menu({buttonName, menuIndex, currRowInd, setCurrRowInd}) {
+  // inside menu
   const [open, setOpen] = useState(false);
-  // press down key, will get 1st item which at index 0
-  const [menuItemActiveIndex, setMenuItemActiveIndex] = useState(-1);
-
   const menuItems = {download: 'download', view: 'view', delete: 'delete'};
-  const menuItemRefs = useRef(createMenuItemRefs(menuItems, parentRowIndex));
+  const [isMenuActioned, setIsMenuActioned] = useState(false);
 
-  // focus
+  /*
   useEffect(() => {
-    if (open && menuItemActiveIndex >= 0 && parentRowIndex !== '') {
-      console.log('focus!!');
-      menuItemRefs.current[parentRowIndex + menuItemActiveIndex].focus();
-    }
-  }, [menuItemActiveIndex, open, parentRowIndex]);
-
-  const clickOutside = event => {
-    if (event.target.nodeName !== 'HTML') {
-      setOpen(false);
-      return;
-    }
-    setOpen(false);
-  };
-
-  // close
-  useEffect(() => {
-    if (open) {
-      document.addEventListener('click', clickOutside);
-    } else {
-      document.removeEventListener('click', clickOutside);
+    // * something is clicked for that button
+    if (isMenuActioned) {
+      // this menu is focus
+      if (currRowInd === menuIndex) {
+        setOpen(!open);
+      } else {
+        //test
+        console.log('never call!!!!!!!!!');
+        setOpen(false);
+      }
     }
 
     return () => {
-      document.removeEventListener('click', clickOutside);
+      // no longer click
+      setIsMenuActioned(false);
     };
-  }, [open]);
+  }, [isMenuActioned, currRowInd, menuIndex, open]);
+  */
 
-  const buttonIconOnClick = (event, parentRowIndex) => {
-    // close it
-    setOpen(!open);
-    // which one to close
-    setCurrRowInd(parentRowIndex);
-  };
-
-  // on the button level
-  const buttonIconKeyDown = (event, parentRowIndex) => {
-    if (event.keyCode === 13) {
-      // Enter pressed
-      console.log('enter is pressed');
-
+  // -- 2 --
+  useEffect(() => {
+    if (currRowInd === menuIndex) {
       setOpen(!open);
-      setCurrRowInd(parentRowIndex);
-    } else if (event.keyCode === 9) {
-      // we cannot tab away
-    } else if (event.keyCode === 40) {
-      //test
-      console.log('down arrow');
-
-      // 38 is up arrow
-
-      // No scrolling
-      event.preventDefault();
-
-      // set to 1st item in 0 index
-      setMenuItemActiveIndex(0);
+    } else {
+      setOpen(false);
     }
-  };
 
-  const itemOnKeyDown = (event, itemIndex) => {
-    if (event.keyCode === 13) {
-      console.log('enter press fire redux event', itemIndex);
-    } else if (event.keyCode === 40) {
-      if (itemIndex < Object.keys(menuItems).length - 1) {
-        console.log('down');
-        setMenuItemActiveIndex(itemIndex + 1);
-      }
-    } else if (event.keyCode === 38) {
-      if (itemIndex > 0) {
-        console.log('up');
-        setMenuItemActiveIndex(itemIndex - 1);
-      }
+    return () => {
+      // test
+      console.log('clean up set open false');
+      setOpen(false);
+    };
+  }, [currRowInd]);
+
+  // -- 1 --
+  useEffect(() => {
+    if (isMenuActioned) {
+      // that menu got focus
+      setCurrRowInd('');
+      setCurrRowInd(menuIndex);
     }
-  };
+
+    return () => {
+      setIsMenuActioned(false);
+    };
+  }, [isMenuActioned]);
 
   const itemOnClick = (event, itemIndex) => {
     // it is mouse click
@@ -115,62 +70,35 @@ function Menu({buttonName, parentRowIndex}) {
           if (event.pageX !== 0 && event.pageY !== 0) {
             //test
             console.log('parent buttonicon onclick: ');
-            buttonIconOnClick(event, parentRowIndex);
+
+            // something clicked
+            setIsMenuActioned(true);
           }
-        }}
-        onKeyDown={event => {
-          //test
-          console.log('parent buttonicon onkeydown: ');
-          buttonIconKeyDown(event, parentRowIndex);
         }}
       >
         {buttonName}
       </button>
 
-      {open && parentRowIndex === currRowInd && (
+      {/*{open && menuIndex === currRowInd && (*/}
+      {open && (
         <ul style={{padding: '5px', margin: '10px', border: '1px solid #ccc'}}>
           {Object.keys(menuItems).map((item, itemIndex) => {
-            if (itemIndex === menuItemActiveIndex)
-              return (
-                <li
-                  tabIndex="0"
-                  key={itemIndex}
-                  style={{
-                    listStyle: 'none',
-                    padding: '5px',
-                    backgroundColor: 'blue'
-                  }}
-                  // put a ref
-                  ref={element =>
-                    (menuItemRefs.current[parentRowIndex + itemIndex] = element)
-                  }
-                  onClick={event => {
-                    itemOnClick(event, itemIndex);
-                  }}
-                  // we want own index
-                  onKeyDown={event => itemOnKeyDown(event, itemIndex)}
-                >
-                  {item}
-                </li>
-              );
-            else
-              return (
-                <li
-                  tabIndex="0"
-                  key={itemIndex}
-                  style={{listStyle: 'none', padding: '5px'}}
-                  ref={element =>
-                    (menuItemRefs.current[parentRowIndex + itemIndex] = element)
-                  }
-                  onClick={event => {
-                    itemOnClick(event, itemIndex);
-                  }}
-                  // we want own index
-                  onKeyDown={event => itemOnKeyDown(event, itemIndex)}
-                >
-                  {item}
-                </li>
-              );
+            return (
+              <li
+                tabIndex="0"
+                key={itemIndex}
+                style={{
+                  listStyle: 'none',
+                  padding: '5px',
+                  backgroundColor: 'blue'
+                }}
+                onClick={event => {
+                  itemOnClick(event, itemIndex);
+                }}
+              >
+                {item}
+              </li>
+            );
           })}
         </ul>
       )}
@@ -179,6 +107,8 @@ function Menu({buttonName, parentRowIndex}) {
 }
 
 function TableElement() {
+  const [currRowInd, setCurrRowInd] = useState('');
+
   const items = [
     {
       file: 'file1',
@@ -203,7 +133,12 @@ function TableElement() {
                 <a href="#">{item.file}</a>
               </td>
               <td style={{border: '1px solid black'}}>
-                <Menu buttonName={item.button} parentRowIndex={index} />
+                <Menu
+                  buttonName={item.button}
+                  menuIndex={index}
+                  currRowInd={currRowInd}
+                  setCurrRowInd={setCurrRowInd}
+                />
               </td>
             </tr>
           );
@@ -214,13 +149,9 @@ function TableElement() {
 }
 
 function App() {
-  const appContextObj = {};
-
   return (
     <>
-      <AppContext.Provider value={appContextObj}>
-        <TableElement />
-      </AppContext.Provider>
+      <TableElement />
     </>
   );
 }
